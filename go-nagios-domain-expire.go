3b24f49c02fd2f3c4176944c8d4f2742b188904e
redacid <redacid@redacid.org.ua>
2017-03-24 11:30:28 +0200

    dev

---
+++ go-nagios-domain-expire.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/likexian/whois-parser-go"
+	//"github.com/fatih/color"
+	"fmt"
+	"flag"
+)
+
+var command string
+
+func init() {
+
+	flag.StringVar(&command, "command", command, "" +
+		"Commands:\n " +
+		"\t\t showConfig - Show configuration file \n" +
+		"\t\t check - Run check domains \n")
+}
+
+
+
+
+func main() {
+	flag.Parse()
+	//pinfo := color.New(color.FgYellow,color.BgBlack)
+	//panounce := color.New(color.FgHiCyan,color.BgBlack)
+	//perror := color.New(color.FgHiRed,color.BgBlack)
+
+	//whois_raw := whois_parser.ReadFile("whois.txt")
+	whois_raw := "ios.in.ua"
+	result, err := whois_parser.Parser(whois_raw)
+	if err == nil {
+		// Print the domain status
+		fmt.Println(result.Registrar.DomainStatus)
+
+		// Print the domain created date
+		fmt.Println(result.Registrar.CreatedDate)
+
+		// Print the domain expiration date
+		fmt.Println(result.Registrar.ExpirationDate)
+
+		// Print the registrant name
+		fmt.Println(result.Registrant.Name)
+
+		// Print the registrant email address
+		fmt.Println(result.Registrant.Email)
+	} else {
+	fmt.Println(err)
+	}
+
+
+
+
+}
